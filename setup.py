3e8ec1418a13a12f35c4dc477205bacba35baf2e
Luwe91 <lukas.weber1991@hotmail.com>
2023-02-20 16:26:27 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="keller_protocol",
+    version="1.0.0",
+    author="Lukas Weber",
+    author_email="engineering@keller-druck.com",
+    description="Python package source for KELLER protocol",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KELLERAGfuerDruckmesstechnik/keller_protocol_python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
