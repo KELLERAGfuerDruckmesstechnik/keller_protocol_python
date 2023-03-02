e880707882400cc143695b013d00f7432e8a2b2e
Luwe91 <79854671+Luwe91@users.noreply.github.com>
2023-03-02 13:18:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/KELLERAGfuerDruckmesstechnik/keller_protocol_python",
-    install_requires=[pyserial]
+    install_requires=["pyserial"]
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
